599ba3f6df8d45eeb6b74be06fb28b03dd3709af
Muhammad Ridwan <bobar717@gmail.com>
2026-06-02 18:44:24 +0700

    feat: implement modul persuratan

---
+++ routes/persuratan/surat.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const { protected } = require("../../middleware/authMiddleware");
+const SuratController = require("../../controllers/persuratan/SuratController");
+const SuratValidator = require("../../middleware/persuratan/SuratValidator");
+const { persuratanUpload } = require("../../middleware/persuratan/persuratanUpload");
+
+// --- Route with Validator ---
+router.post("/", protected, persuratanUpload, SuratValidator.create, SuratController.create);
+
+router.post("/disposisi/:id", protected, persuratanUpload, SuratValidator.disposisi, SuratController.disposisi);
+
+router.put("/status/:id", protected, SuratValidator.updateStatus, SuratController.updateStatus);
+
+// --- Standard Routes (Without using Validator) ---
+router.get("/", protected, SuratController.index);
+router.get("/tracking/:id", protected, SuratController.getTracking);
+router.get("/:id", protected, SuratController.detail);
+router.delete("/:id", protected, SuratController.delete);
+
+module.exports = router;
--- routes/routes.js
+++ routes/routes.js
@@ -32,15 +32,16 @@ const validasiRoutes = require("./validasi/validasiRoutes");
 const laporanRoutes = require("./laporan/laporanRoutes");
 const ruanganRoutes = require("./ruangan/ruanganRoutes");
 const ujianRoutes = require("./ujian/ujianRoutes");
-const cbtRoutes = require('./Authentication/cbtRoutes');
+const cbtRoutes = require("./Authentication/cbtRoutes");
 const siakRoutes = require("./siak/siakRoute");
 // const parentsRoutes = require("./parents/parentsRoutes");
 const absensiExternalRoutes = require("./absensi-external/indexRouter");
+const suratRoutes = require("./persuratan/surat");
 const router = exporess.Router();
 
 router.use("/auth", authRoutes);
 router.use("/admin", adminRoutes);
-router.use('/cbt', cbtRoutes);
+router.use("/cbt", cbtRoutes);
 router.use("/users", usersRoutes);
 router.use("/profile", profileRoutes);
 router.use("/kualifikasi", kualifikasiRoutes);
@@ -74,7 +75,7 @@ router.use("/ujian", ujianRoutes);
 router.use("/siak", siakRoutes);
 // router.use("/parents", parentsRoutes);
 router.use("/absensi-external", absensiExternalRoutes);
-
+router.use("/surat", suratRoutes);
 
 router.use("/help", helpRoutes);
 router.get("/", (req, res) => {
